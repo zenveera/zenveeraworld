26766fc75d62ffb480bcd4962c9c7f949115db75
zenveera <zenveeraworld2511@gmail.com>
2026-08-06 19:07:55 +0530

    Update catalog.js

--- js/catalog.js
+++ js/catalog.js
@@ -1407,7 +1407,6 @@ quickAddBtn.addEventListener("click", async (e) => {
     quickAddBtn.classList.add("added");
     quickAddBtn.innerHTML = `<i class="fa-solid fa-check"></i> Added`;
 });
-
       grid.appendChild(card);
 
       if (revealObserver) {
