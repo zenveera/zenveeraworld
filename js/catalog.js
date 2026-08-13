dc0efaedd3dd34c284710088e2f32c21e3067969
zenveera <zenveeraworld2511@gmail.com>
2026-08-13 15:48:25 +0530

    Update catalog.js

--- js/catalog.js
+++ js/catalog.js
@@ -2500,7 +2500,7 @@ ${escapeHtml(c.name)}
 
   if (openCheckoutBtn && checkoutModal) {
     openCheckoutBtn.addEventListener("click", () => {
-      alert("button click");
+      
 
       const total = cart.reduce((sum, item) => {
         return sum + Number(item.price || 0) * Number(item.qty || 0);
